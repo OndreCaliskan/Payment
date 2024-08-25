74e7feac4735ad2bc4d8d67e12ecbc3a5b66ba30
Bunyaminpilsim <bunyaminpilsim@gmail.com>
2024-08-25 14:43:16 +0300

    category sınıfı güncellendi category admin tarafı tasarlandı

---
+++ Api/Payment.DataAccessLayer/Migrations/20240825114122_updated_category_mig.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Payment.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class updated_category_mig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateTime",
+                table: "Categories",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreateUser",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdateTime",
+                table: "Categories",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdateUser",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateTime",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "CreateUser",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateTime",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateUser",
+                table: "Categories");
+        }
+    }
+}
---
+++ Frontend/Payment.WebUI/Controllers/CategoryController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Payment.WebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+
+            return View();
+        }
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCategory(string addCategoryViewModel)
+        {
+
+            return View();
+        }
+
+
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory(int id)
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> UpdateCategory(string updateCategoryViewModel)
+        {
+
+
+            return View();
+        }
+    }
+}
