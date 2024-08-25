352f39070f38baaf3bc8a969ba3660fa6f6296e9
Taha Çalık <receptaha61@icloud.com>
2024-08-25 10:53:21 +0300

    tahastos

---
+++ Api/Payment.EntityLayer/Concrete/Armut.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment.EntityLayer.Concrete
+{
+    public class Armut
+    {
+        public int ArmutID { get; set; }
+
+    }
+}
